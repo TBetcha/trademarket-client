4416253978bcefacd714eade75e33c769bfbfde5
Tbetcha <troy.boettger@gmail.com>
2025-05-29 16:35:45 -0400

    input fields are correct for sign up

--- app/components/signupform.tsx
+++ app/components/signupform.tsx
@@ -14,8 +14,10 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroupItem, RadioGroup } from "@/components/ui/radio-group";
 
-const addressTypeEnum = z.enum(["Home", "Work", "Other"]);
+const addressTypeEnum = z.enum(["home", "work", "other"]);
 
 const addressSchema = z.object({
   line1: z
@@ -25,37 +27,42 @@ const addressSchema = z.object({
     .min(1, { message: "Street is required" }),
   line2: z.string().trim().toLowerCase().optional(),
   city: z.string().trim().toLowerCase().min(1, { message: "City is required" }),
-  postalCode: z.string().trim().min(5, { message: "Postal code is required" }),
+  postalCode: z
+    .string()
+    .trim()
+    .min(5)
+    .max(10, { message: "Postal code is required" }),
   state: z.string().min(2).max(2, { message: "Country is required" }),
   type: addressTypeEnum,
 });
 
-const signUpFormSchema = z
-  .object({
-    firstName: z.string().trim().toLowerCase().min(2, {
-      message: "First name must be at least 2 characters.",
-    }),
-    lastName: z.string().trim().toLowerCase().min(2, {
-      message: "Last name must be at least 2 characters.",
-    }),
+const signUpFormSchema = z.object({
+  firstName: z.string().trim().toLowerCase().min(2, {
+    message: "First name must be at least 2 characters.",
+  }),
+  lastName: z.string().trim().toLowerCase().min(2, {
+    message: "Last name must be at least 2 characters.",
+  }),
 
-    email: z.string().trim().toLowerCase().email({
-      message: "Email address is invalid",
-    }),
+  email: z.string().trim().toLowerCase().email({
+    message: "Email address is invalid",
+  }),
+  dateOfBirth: z.string().trim(),
 
-    password: z.string().trim().min(6, {
-      message: "Password must be at least 6 characters",
-    }),
-    confirm: z.string().trim(),
-    dateOfBirth: z.date().refine((date) => !isNaN(date.getTime()), {
-      message: "Invalid date",
-    }),
-    address: addressSchema,
-  })
-  .refine((data) => data.password === data.confirm, {
-    message: "Passwords don't match",
-    path: ["confirm"], // path of error
-  });
+  password: z.string().trim().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
+  //confirm: z.string().trim(),
+  //dateOfBirth: z.date().refine((date) => !isNaN(date.getTime()), {
+  //  message: "Invalid date",
+  //}),
+  address: addressSchema,
+});
+//.refine((data) => data.password === data.confirm, {
+//  message: "Passwords don't match",
+//  path: ["confirm"], // path of error
+//});
+
 export function SignUpForm() {
   // 1. Define your form.
   const form = useForm<z.infer<typeof signUpFormSchema>>({
@@ -64,22 +71,22 @@ export function SignUpForm() {
       firstName: "",
       lastName: "",
       email: "",
+      dateOfBirth: "",
       password: "",
-      confirm: "",
       address: {
         line1: "",
         line2: "",
         city: "",
         postalCode: "",
         state: "",
-        type: "Home",
+        type: "home",
       },
     },
   });
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="firstName"
@@ -110,12 +117,12 @@ export function SignUpForm() {
 
         <FormField
           control={form.control}
-          name="lastName"
+          name="dateOfBirth"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Last Name</FormLabel>
+              <FormLabel>Date of Birth</FormLabel>
               <FormControl>
-                <Input placeholder="Doe" {...field} />
+                <Input placeholder="01/01/2001" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -124,7 +131,7 @@ export function SignUpForm() {
 
         <FormField
           control={form.control}
-          name="lastName"
+          name="email"
           render={({ field }) => (
             <FormItem>
               <FormLabel>Email</FormLabel>
@@ -144,6 +151,113 @@ export function SignUpForm() {
               <FormLabel>Password</FormLabel>
               <FormControl>
                 <Input placeholder="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.line1"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Line 1</FormLabel>
+              <FormControl>
+                <Input placeholder="123 Main St." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.line2"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Line 2</FormLabel>
+              <FormControl>
+                <Input placeholder="Apt 4b" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.city"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>City</FormLabel>
+              <FormControl>
+                <Input placeholder="New York" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.state"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>State</FormLabel>
+              <FormControl>
+                <Input placeholder="NY" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.postalCode"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Postal Code</FormLabel>
+              <FormControl>
+                <Input placeholder="10004" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="address.type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Address Type</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="home" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Home</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="office" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Office</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="other" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Other</FormLabel>
+                  </FormItem>
+                </RadioGroup>
               </FormControl>
               <FormMessage />
             </FormItem>
